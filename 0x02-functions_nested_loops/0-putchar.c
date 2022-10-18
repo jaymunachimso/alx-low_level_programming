02cd704837022f67532b4460b51ab1ee5f58df7d
Jay Munachimso <113600484+jaymunachimso@users.noreply.github.com>
2022-10-18 15:18:12 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
-#include "main.h"
+#include "putchar.h"
 
 /**
- * main -  prints Holberton, followed by a new line
+ * main -  prints putchar, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('A');
-	_putchar('l');
-	_putchar('x');
-	_putchar('s');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
 	_putchar('c');
 	_putchar('h');
-	_putchar('o');
-	_putchar('o');
-	_putchar('l');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
