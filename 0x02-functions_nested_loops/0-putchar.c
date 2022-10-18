6601080a3cda9c296334d6c7ad0ce8832d269c1c
Jay Munachimso <113600484+jaymunachimso@users.noreply.github.com>
2022-10-18 15:57:38 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-=======
-#include "putchar.h"
+#include "main.h"
 
 /**
  * main -  prints putchar, followed by a new line
@@ -18,9 +16,4 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
-<<<<<<< HEAD
 }
->>>>>>> 02cd704837022f67532b4460b51ab1ee5f58df7d
-=======
-}
->>>>>>> c5a7343f446d5971de6e6effd53def5ba17f05d4
