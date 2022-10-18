a0b9b7883fc0f9e588f01a594fc3af9dff77e499
Jay Munachimso <113600484+jaymunachimso@users.noreply.github.com>
2022-10-18 12:24:47 +0100

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "Alxschool.h"
+
+/**
+ * main -  prints Holberton, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('A');
+	_putchar('l');
+	_putchar('x');
+	_putchar('s');
+	_putchar('c');
+	_putchar('h');
+	_putchar('o');
+	_putchar('o');
+	_putchar('l');
+	_putchar('\n');
+
+	return (0);
+}
